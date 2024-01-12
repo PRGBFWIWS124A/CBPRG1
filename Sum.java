cba04b53eed8541a2be16390ab76780abcce179f
bfwi124abo <christian.boll@edu.fhdw.de>
2024-01-12 12:44:01 +0100

    Commit 3 - Sum.java

--- Sum.java
+++ Sum.java
@@ -4,10 +4,10 @@ public class Sum {
         if(args.length >= 2)
         {
             try {
-                int Z1 = Integer.parseInt(args[0]);
-                int Z2 = Integer.parseInt(args[1]);
-                System.out.println(Z1 + " + " + Z2 + " = " + (Z1 + Z2));
-            } catch (NumberFormatException e) {
+                int temp = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
+                String test = String.valueOf(temp);
+                System.out.println(test);
+            } catch (Exception e) {
                 System.err.println("Both arguments must be of type Integer!");
             }
         }
