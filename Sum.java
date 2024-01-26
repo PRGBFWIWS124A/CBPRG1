dabe82a9d02d00631fc23a07e75fbc44e44b183d
bfwi124abo <christian.boll@edu.fhdw.de>
2024-01-26 09:38:39 +0100

    Commit 5 - Sum.java

--- Sum.java
+++ Sum.java
@@ -7,6 +7,12 @@ public class Sum {
                 int temp = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
                 String test = String.valueOf(temp);
                 System.out.println(test);
+                //Funktionen
+                output("Summe aus der Produkt-Funktion: " + product(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
+                output("Quadratsumme der beiden Werte: " + squaresum(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
+                //Prozeduren
+                output("TEST");
+                warning();
             } catch (Exception e) {
                 System.err.println("Both arguments must be of type Integer!");
             }
@@ -14,5 +20,24 @@ public class Sum {
         else {
             System.err.println("Missing arguments: Provide two Integers!");
         }
+    }
+
+    public static int product(int x, int y)
+    {
+        return x * y;
+    }
+
+    public static int squaresum(int x, int y)
+    {
+        return (x * x) + (y * y);
+    }
+
+    public static void output(String content)
+    {
+        System.out.println(content);
+    }
+
+    public static void warning() {
+        System.out.println("WARNUNG");
     }
 }
